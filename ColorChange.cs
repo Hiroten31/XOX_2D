fcab9d15d371de759e032f232cd9438248215809
Hiroten (Mikołaj Piech) <97809912+Hiroten31@users.noreply.github.com>
2024-03-08 21:34:47 +0100

    Adding first script
    My first working C# script for Unity Game Engine. Changing color of background by corresponding keys.

---
+++ ColorChange.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer MySprite;
+    // Start is called before the first frame update
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.R)) {
+            GetComponent<SpriteRenderer>().material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<SpriteRenderer>().material.color = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<SpriteRenderer>().material.color = Color.blue;
+        }
+    }
+}
