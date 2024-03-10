ef3eca05131750e064b5500a26861265e570642f
Hiroten (Mikołaj Piech) <97809912+Hiroten31@users.noreply.github.com>
2024-03-11 00:26:28 +0100

    Update TextureChange.cs

--- TextureChange.cs
+++ TextureChange.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class TextureChange : MonoBehaviour {
 
-    public Material myMaterial;
-
     // Try doing it by a list? Like, if I add one, I can add another and another and ...
-    public Texture myTexture1;
-    public Texture myTexture2;
-    public Texture myTexture3;
-    public Texture myTexture4;
+    public Sprite mySprite1;
+    public Sprite mySprite2;
+    public Sprite mySprite3;
+    public Sprite mySprite4;
 
 
     // Start is called before the first frame update
@@ -21,21 +19,20 @@ public class TextureChange : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         if (Input.GetKeyDown(KeyCode.W)) {
-            myMaterial.mainTexture = myTexture1;
+            GetComponent<SpriteRenderer>().sprite = mySprite1;
             Debug.Log("KEYCODE.W!");
         }
         if (Input.GetKeyDown(KeyCode.A)) {
-            myMaterial.mainTexture = myTexture2;
+            GetComponent<SpriteRenderer>().sprite = mySprite2;
             Debug.Log("KEYCODE.A!");
         }
         if (Input.GetKeyDown(KeyCode.S)) {
-            myMaterial.mainTexture = myTexture3;
+            GetComponent<SpriteRenderer>().sprite = mySprite3;
             Debug.Log("KEYCODE.S!");
         }
         if (Input.GetKeyDown(KeyCode.D)) {
-            myMaterial.mainTexture = myTexture4;
+            GetComponent<SpriteRenderer>().sprite = mySprite4;
             Debug.Log("KEYCODE.D!");
         }
-        // It is changing material, but it doesn't show...
     }
 }
