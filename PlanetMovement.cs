299f6055cc921985e5de8a3f74624e96a496a4a9
Hiroten (Mikołaj Piech) <97809912+Hiroten31@users.noreply.github.com>
2024-03-12 21:50:38 +0100

    Update PlanetMovement.cs
    Fixed planet going into endless space by infinite incrementation of X and Y positions

--- PlanetMovement.cs
+++ PlanetMovement.cs
@@ -7,13 +7,13 @@ public class PlanetMovement : MonoBehaviour {
 
     public Transform target;
     public float speed = 2f;
-    public float radius = 5;
+    public float radius = 1f;
     public float angle = 0f;
 
     // Update is called once per frame
     void Update() {
-        float x = 2 + target.position.x + Mathf.Cos(angle) * radius;
-        float y = 2 + target.position.y + Mathf.Sin(angle) * radius;
+        float x = Mathf.Cos(angle) * radius;
+        float y = Mathf.Sin(angle) * radius;
         transform.position = new Vector3(x, y, 0);
         angle += speed * Time.deltaTime;
     }
