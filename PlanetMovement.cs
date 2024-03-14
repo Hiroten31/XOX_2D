e339380f0c4dc37d2468300eb635b9b54767bbf0
Hiroten (Mikołaj Piech) <97809912+Hiroten31@users.noreply.github.com>
2024-03-14 22:42:50 +0100

    Update PlanetMovement.cs
    Added rotation, moved getting random values into function for testing and better performance.

--- PlanetMovement.cs
+++ PlanetMovement.cs
@@ -1,34 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
 public class PlanetMovement : MonoBehaviour {
-
-
     public Transform target;
     public float speed;
     public float radius;
     public float angle = 0f;
     public float startLocX;
     public float startLocY;
+    public float scale;
+    public float rotation;
 
     private void Start() {
-        startLocX = Random.Range(-10f, 10f);
-        startLocY = Random.Range(-6f, 6f);
-        radius = Random.Range(1f, 5f);
-        speed = Random.Range(1f, 3f);
+        GetRandomValues();
     }
 
     // Update is called once per frame
     void Update() {
         // Here add function to set a scale for a random planet.
-        /*float random = Random.Range(1f, 5f);
-        transform.localScale = new Vector3(Mathf.Cos(angle), Mathf.Cos(angle), 0);*/
         float x = startLocX + Mathf.Cos(angle) * radius;
         float y = startLocY + Mathf.Sin(angle) * radius;
         transform.position = new Vector3(x, y, 0);
-        transform.rotation = Quaternion.Euler(0, 0, angle * 20);
+        transform.rotation = Quaternion.Euler(0, 0, rotation + angle * 20);
         angle += speed * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.R)){
+            GetRandomValues();
+        }
+    }
+
+    void GetRandomValues() {
+        startLocX = Random.Range(-10f, 10f);
+        startLocY = Random.Range(-6f, 6f);
+        radius = Random.Range(1f, 5f);
+        speed = Random.Range(0.25f, 1.5f);
+        scale = Random.Range(0.1f, 0.6f);
+        rotation = Random.Range(0f, 360f);
+        transform.localScale = new Vector3(scale, scale, 0);
     }
 }
