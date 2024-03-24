5718be68ba57767293e34150fef00e5d7c9446ee
Hiroten (Mikołaj Piech) <97809912+Hiroten31@users.noreply.github.com>
2024-03-24 21:11:04 +0100

    Update GridManager.cs
    fixed even numbers offset.

--- GridManager.cs
+++ GridManager.cs
@@ -6,12 +6,15 @@ using UnityEngine.Tilemaps;
 public class GridManager : MonoBehaviour {
 
     [SerializeField] private int _square;
-
     [SerializeField] private Tile _tilePrefab;
     void GenerateGrid() {
-        for (int x = -(_square / 2); x < _square - (_square / 2); x++) {
-            for(int y = -(_square / 2); y < _square - (_square / 2); y++) {
-                var spawnedTile = Instantiate(_tilePrefab, new Vector2(x, y), Quaternion.identity);
+        float offsetPosition = _square / 2;
+        if (_square%2 == 0) {
+            offsetPosition -= 0.5f;
+        }
+        for (int x = 0; x < _square; x++) {
+            for(int y = 0; y < _square; y++) {
+                var spawnedTile = Instantiate(_tilePrefab, new Vector2(x - offsetPosition, y - offsetPosition), Quaternion.identity);
                 spawnedTile.name = $"Tile {x} {y}";
                 var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
                 spawnedTile.Init(isOffset);
