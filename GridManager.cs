0f2c6ef7fbfe87f855d3424082c4025fa7eff977
Hiroten (Mikołaj Piech) <97809912+Hiroten31@users.noreply.github.com>
2024-03-26 22:50:17 +0100

    Update GridManager.cs
    I change it a little bit so the "Play" button will have control over how many squares there will be

--- GridManager.cs
+++ GridManager.cs
@@ -1,24 +1,20 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 public class GridManager : MonoBehaviour {
 
-    [SerializeField] private int _square;
     [SerializeField] private Tile _tilePrefab;
 
-    private Dictionary<Vector2, Tile> _tiles;
-    
-    void GenerateGrid() {
+    public Dictionary<Vector2, Tile> _tiles;
+
+    public void GenerateGrid(int _square) {
         float offsetPosition = _square / 2;
-        if (_square%2 == 0) {
+        if (_square % 2 == 0) {
             offsetPosition -= 0.5f;
         }
         _tiles = new Dictionary<Vector2, Tile>();
         for (int x = 0; x < _square; x++) {
-            for(int y = 0; y < _square; y++) {
+            for (int y = 0; y < _square; y++) {
                 var spawnedTile = Instantiate(_tilePrefab, new Vector2(x - offsetPosition, y - offsetPosition), Quaternion.identity);
                 spawnedTile.name = $"Tile {x} {y}";
                 var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
@@ -28,13 +24,9 @@ public class GridManager : MonoBehaviour {
             }
         }
     }
-    // Start is called before the first frame update
-    void Start() {
-        GenerateGrid();
-    }
 
     public Tile GetTileAtPosition(Vector2 position) {
-        if(_tiles.TryGetValue(position, out Tile tile)) 
+        if (_tiles.TryGetValue(position, out Tile tile))
             return tile;
         return null;
     }
