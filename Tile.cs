3a772b6d5b5763411a3064750e2900d917697e49
Hiroten (Mikołaj Piech) <97809912+Hiroten31@users.noreply.github.com>
2024-03-26 22:46:36 +0100

    Update Tile.cs
    added placing O's and X's.

--- Tile.cs
+++ Tile.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Tile : MonoBehaviour {
+
     [SerializeField] private Color _baseColor, _offsetColor;
     [SerializeField] private SpriteRenderer _renderer;
     [SerializeField] private GameObject _highlight;
+    [SerializeField] private GameObject _O;
+    [SerializeField] private GameObject _X;
 
     public void Init(bool isOffset) { 
         _renderer.color = isOffset ? _offsetColor : _baseColor;
@@ -13,6 +16,16 @@ public class Tile : MonoBehaviour {
 
     private void OnMouseEnter() {
         _highlight.SetActive(true);
+    }
+
+    private void OnMouseDown() {
+        if (_O.activeSelf == false) {
+            _O.SetActive(true);
+            _X.SetActive(false);
+        } else {
+            _X.SetActive(true);
+            _O.SetActive(false);
+        }
     }
 
     private void OnMouseExit() {
