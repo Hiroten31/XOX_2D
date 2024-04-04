6905410d458bb605daf91ac85f7c9258785b764c
Hiroten (Mikołaj Piech) <97809912+Hiroten31@users.noreply.github.com>
2024-04-04 23:17:54 +0200

    Update SliderScript.cs
    Fixed no value if directly went into "Play"

--- SliderScript.cs
+++ SliderScript.cs
@@ -9,8 +9,9 @@ public class SliderScript : MonoBehaviour {
     [SerializeField] private Slider _slider;
     [SerializeField] private TextMeshProUGUI _sliderValueText;
 
-    public static int value;
+    public static int value = 3;
 
+    // Start is called before the first frame update
     void Start() {
         _slider.onValueChanged.AddListener((v) => {
             _sliderValueText.text = v.ToString("0");
