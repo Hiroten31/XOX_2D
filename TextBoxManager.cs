5fd3b668edb173b3b90f96f1584737f1c2829038
Hiroten (Mikołaj Piech) <97809912+Hiroten31@users.noreply.github.com>
2024-05-05 09:41:50 +0200

    Update TextBoxManager.cs
    Cleared some useless stuff and added stopping coroutine

--- TextBoxManager.cs
+++ TextBoxManager.cs
@@ -6,28 +6,26 @@ using TMPro;
 public class TextBoxManager : MonoBehaviour {
 
     public TextMeshProUGUI text;
-    public string[] textCharacters;
     public float textSpeed;
-
-    private int index;
 
     private void Start() {
         text.text = string.Empty;
     }
 
+    public void EmptyTextBox() {
+        StopAllCoroutines();
+        text.text = string.Empty;
+    }
+
     public void StartTextBox(string msg) {
-        index = 0;
         StartCoroutine(TypeLine(msg));
     }
 
     public IEnumerator TypeLine(string msg) {
-        foreach (char c in msg.ToCharArray()) {
+        foreach (char c in msg) {
             text.text += c;
             yield return new WaitForSeconds(textSpeed);
         }
     }
 
-    void Update() {
-        
-    }
 }
