1e0d67fc36bab4f4629a583540e8fda25f2b4d98
Hiroten (Mikołaj Piech) <97809912+Hiroten31@users.noreply.github.com>
2024-05-07 23:14:55 +0200

    Added gameState

--- GameManager.cs
+++ GameManager.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
 
     public static GameManager Instance;
+    //gameState to control what player is currently doing
+    //0 is menu
+    //1 is game
+    //2 is game + menu?? -> (player won, nothing dissapears and game waits for player to click menu option)
+    public static int gameState = 0;
 
     public int gridSize = 3;
 
