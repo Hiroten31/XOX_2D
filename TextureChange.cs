2e174e353e2bbce9b00d69b7288e841b65a45283
Hiroten (Mikołaj Piech) <97809912+Hiroten31@users.noreply.github.com>
2024-05-19 23:46:02 +0200

    IEnumarator solution (somewhat working)

--- TextureChange.cs
+++ TextureChange.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TextureChange : MonoBehaviour {
@@ -9,45 +10,53 @@ public class TextureChange : MonoBehaviour {
 
     public Sprite mySprite;
     public float timeToFade;
-    private bool change = false;
 
-    private Color FadeOut;
-    private Color FadeIn;
-    private SpriteRenderer spriteToFadeOut;
-    private SpriteRenderer spriteToFadeIn;
-
-    // Start is called before the first frame update
-    void Start() {
-
-    }
-
-    // Update is called once per frame
-    void Update() {
+    private void Update() {
         if (Input.GetKeyDown(KeyCode.Space)) {
-            TextureFade();
-        }
-        if(change) {
-            FadeOut.a -= Time.deltaTime * timeToFade;
-            spriteToFadeOut.color = FadeOut;
-            FadeIn.a += Time.deltaTime * timeToFade;
-            spriteToFadeIn.color = FadeIn;
-            if (FadeIn.a == 1 && FadeOut.a == 0) {
-                change = false;
-            }
+            StartTextureChange();
         }
     }
-    
-    void TextureFade() {
-        change = true;
+    void StartTextureChange() {
+        StartCoroutine(TextureFadeOut(GetComponent<SpriteRenderer>()));
         int random;
         do {
             random = Random.Range(0, mySprites.Count);
         } while (mySprites[random] == GetComponent<SpriteRenderer>().sprite);
-        SpriteRenderer spriteToFadeOut = GetComponent<SpriteRenderer>();
-        Color FadeOut = spriteToFadeOut.color;
-        GetComponent<SpriteRenderer>().sprite = mySprites[random];
-        SpriteRenderer spriteToFadeIn = GetComponent<SpriteRenderer>();
-        Color FadeIn = spriteToFadeIn.color;
-        GetComponent<SpriteRenderer>().sprite = spriteToFadeOut.sprite;
+        SpriteRenderer tempSprite = Instantiate(GetComponent<SpriteRenderer>(), transform);
+        tempSprite.sprite = mySprites[random];
+        StartCoroutine(TextureFadeIn(tempSprite));
+    }
+    
+    IEnumerator TextureFadeOut(SpriteRenderer _sprite) {
+        Debug.Log("Started TextureFadeOut()");
+        Color tempColor = _sprite.color;
+        while(tempColor.a <= 1f) {
+            tempColor.a -= Time.deltaTime / timeToFade;
+            _sprite.color = tempColor;
+            if(tempColor.a <= 0f) {
+                tempColor.a = 0.0f;
+            }
+            yield return null;
+        }
+        _sprite.color = tempColor;
+    }
+
+    IEnumerator TextureFadeIn(SpriteRenderer _sprite) {
+        Debug.Log("Started TextureFadeIn()");
+        Color tempColor = _sprite.color;
+        _sprite.color = new Color(255, 255, 255, 0);
+        //_sprite.name = "Child Sprite";
+        //Debug.Log("Parent name: " + _sprite.transform.parent.name);
+        while (tempColor.a >= 0f) {
+            tempColor.a += Time.deltaTime / timeToFade;
+            _sprite.color = tempColor;
+            if (tempColor.a >= 1f) {
+                tempColor.a = 1.0f;
+                //SpriteRenderer parentSprite =_sprite.transform.parent.GetComponent<SpriteRenderer>();
+                //parentSprite.sprite = _sprite.sprite;
+            }
+            yield return null;
+        }
+        _sprite.color = tempColor;
     }
 }
